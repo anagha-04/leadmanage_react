8b12238e96b847cf48fe1f87d0f750bb537a3070
anagha-04 <anaghanilkumar04@gmail.com>
2026-01-21 10:19:10 +0530

    cj

--- src/components/Studentlist.jsx
+++ src/components/Studentlist.jsx
@@ -5,14 +5,16 @@ function Studentlist() {
 
 
   useEffect(()=>{
-     fetch("http://127.0.0.1:8000/")
-  })
+     fetch("http://127.0.0.1:8000/add/").then((res) => res.json()).then((data)=> setstudents(data))
+
+  },[])
 
 
   return (
     <div>
 
         <h1>Studentlist</h1>
+        <p>Total Length: {students.length}</p>
     </div>
   )
 }
