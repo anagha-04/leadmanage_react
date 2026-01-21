883243b8446358ef86a29a2fa03fe447ea35a075
anagha-04 <anaghanilkumar04@gmail.com>
2026-01-21 09:58:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Studentlist from './components/Studentlist'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Student Lead</h1>
+      <Studentlist> </Studentlist>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Studentlist.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+function Studentlist() {
+  const [studentName,setstudentName]= useState("")
+  const[qualification,setqualification]=useState("")
+  const[source,setsource]= useState("")
+  return (
+    <div>
+
+        <h1>Studentlist</h1>
+    </div>
+  )
+}
+
+export default Studentlist
