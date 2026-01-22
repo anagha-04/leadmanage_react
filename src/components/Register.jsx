002904bb76611cc64c798af1ea344f91870b49eb
anagha-04 <anaghanilkumar04@gmail.com>
2026-01-22 10:03:59 +0530

    reg

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -13,15 +13,19 @@ function Register() {
             [e.target.name]:e.target.value
         })
     }
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+    }
   return (
     <div>
         <h2>Register</h2>
         <form action="">
-            <input type="text" name='username' placeholder='enter username' />
+            <input type="text" name='username' placeholder='enter username'onChange={handleChange} />
             <br /><br />
-            <input type="password" name='password' placeholder='enter password'/>
+            <input type="password" name='password' placeholder='enter password' onChange={handleChange}/>
             <br /><br />
-            <input type="email" name='email' placeholder='enter email id' />
+            <input type="email" name='email' placeholder='enter email id' onChange={handleChange} />
             <br /><br />
             <button type='submit'>Register</button>
         </form>
