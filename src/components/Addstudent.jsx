e90357421ea63d7c8f8d64adfe747e57af1f1fed
anagha-04 <anaghanilkumar04@gmail.com>
2026-01-23 10:13:50 +0530

    add

--- src/components/Addstudent.jsx
+++ src/components/Addstudent.jsx
@@ -2,18 +2,18 @@ import React, { useState } from 'react'
 
 function Addstudent() {
 
-    const[student_name,setStudentName] = useState("")
-    const[Qualification,setQualification] = useState("")
-    const[ source,setSource] = useState("")
+    const [student_name, setStudentName] = useState("")
+    const [Qualification, setQualification] = useState("")
+    const [source, setSource] = useState("")
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e) => {
         e.preventDefault()
 
         const token = localStorage.getItem("token")
 
-        fetch("http://127.0.0.1:8000/add/",{
+        fetch("http://127.0.0.1:8000/add/", {
             method: "POST",
-            headers:{
+            headers: {
                 "Content-Type": "application/json",
                 "Authorization": `Token ${token}`
             },
@@ -24,19 +24,40 @@ function Addstudent() {
 
             })
         })
-        .then(res => res.json())
-        .then(data =>{
-            alert("student created successfully")
-            setStudentName("")
-            setQualification("")
-            setSource("friend")
-            
-        })
+            .then(res => res.json())
+            .then(data => {
+                alert("student created successfully")
+                setStudentName("")
+                setQualification("")
+                setSource("friend")
+
+            })
     }
 
-  return (
-    <div>Addstudent</div>
-  )
+    return (
+        <div>
+            <h2>Create Student</h2>
+            <form onSubmit={handleSubmit}>
+                <input placeholder='Student Name'
+                    value={student_name}
+                    onChange={(e) => setStudent_name(e.target.value)}
+                />
+
+                <input placeholder='Qualification'
+                    value={qualification}
+                    onChange={(e) => setQualification(e.target.value)}
+                />
+
+                <select value={source} onChange={(e) => setSource(e.target, value)}>
+                    <option value="friend">Friend</option>
+                    <option value="instagram">Instagram</option>
+                    <option value="own">Own</option>
+                </select>
+
+                <button type='submit'>Create</button>
+            </form>
+        </div>
+    )
 }
 
 export default Addstudent
